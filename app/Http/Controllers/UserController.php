9df79de89a35ab7d92f4eae46ed191a79f9ebb9e
Jonathiux <2122200423@soy.utj.edu.mx>
2023-10-27 18:39:08 -0600

    Agrega accion de registrar usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,48 +65,50 @@ class UserController extends Controller
     }
 
     public function register(Request $request){
-        $json = $request->input('json', null);
-        $params = json_decode($json); //Permite convertir a formato de objeto de php lo que llegue
-        $email = (!is_null($json) && isset($params->email)) ? $params->email : null;
-        $name = (!is_null($json) && isset($params->name)) ? $params->name : null;
-        $role =  'user';
-        $password =  (!is_null($json) && isset($params->password)) ? $params->password : null;
-        if(!is_null($email) && !is_null($password) && !is_null($name)){
-            //Crear el usuario
-            $user = new User();
-            $user->email=$email;
-            $user->name=$name;
-            $pwd = hash('sha256', $password);
-            $user->password = $pwd;
-            //Comprobar usuario duplicado
-            $isset_user = User::where('email','=', $email)->get();
-            if(count($isset_user)==0){
-                //Guardar el usuario
-                $user->save();
-                $data = array(
-                    'status'=>'success',
-                    'codigo' => 200,
-                    'message'=>'Usuario Registrado correctamente'
-                );
-            }else{
-                //No guardarlo
-                $data = array(
-                    'status'=>'error',
-                    'codigo' => 400,
-                    'message'=>'Usuario Duplicado, no puede registrarse'
-                );
-                }
-     return response()->json($data, 200);
-     
-        }else{
-            $data = array(
-                'status'=>'error',
-                'codigo' => 400,
-                'message'=>'Usuario no Creado'
-            );
-            return response()->json($data, 200);
-        }
-        echo "Acción registro"; die();
+          //Recoger el post
+   $json = $request->input('json', null);
+   $params = json_decode($json); //Permite convertir a formato de objeto de php lo que llegue
+   $email = (!is_null($json) && isset($params->email)) ? $params->email : null;
+   $name = (!is_null($json) && isset($params->name)) ? $params->name : null;
+   
+   $role =  'user';
+   $password =  (!is_null($json) && isset($params->password)) ? $params->password : null;
+   if(!is_null($email) && !is_null($password) && !is_null($name)){
+       //Crear el usuario
+       $user = new User();
+       $user->email=$email;
+       $user->name=$name;
+       $pwd = hash('sha256', $password);
+       $user->password = $pwd;
+       //Comprobar usuario duplicado
+       $isset_user = User::where('email','=', $email)->get();
+       if(count($isset_user)==0){
+           //Guardar el usuario
+           $user->save();
+           $data = array(
+               'status'=>'success',
+               'codigo' => 200,
+               'message'=>'Usuario Registrado correctamente'
+           );
+	
+       }else{
+           //No guardarlo
+           $data = array(
+               'status'=>'error',
+               'codigo' => 400,
+               'message'=>'Usuario Duplicado, no puede registrarse'
+           );
+           }
+return response()->json($data, 200);
+
+   }else{
+       $data = array(
+           'status'=>'error',
+           'codigo' => 400,
+           'message'=>'Usuario no Creado'
+       );
+       return response()->json($data, 200);
+   }
      }
 
      public function login(Request $request){
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
